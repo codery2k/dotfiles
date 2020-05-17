f6b6ed4d001bdbf3d047314734010e9e3285ab57
codery2k <95smiglani@gmail.com>
2020-05-17 13:05:10 +0530

    Update stub with basic structure

--- random_unsplash_wallpaper.py
+++ random_unsplash_wallpaper.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 
+WALLPAPER_DIR=""
+UNSPLASH_SOURCE_API_URL= ""
+SEARCH_STRING=""
+
+def get_display_resolution():
+    pass
+
+def download_image():
+    pass
+
+def process_image():
+    pass
+
+def set_wallpaper():
+    pass
+
+if __name__ == "__main__":
+    get_display_resolution()
+    download_image()
+    process_image()
+    set_wallpaper()

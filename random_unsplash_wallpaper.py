2686ca927f46b1546436ee819b2a03ac496ae1f5
Saurabh.Miglani <95smiglani@gmail.com>
2020-05-20 01:44:47 +0530

    Fix style: remove unnecessary comments

--- random_unsplash_wallpaper.py
+++ random_unsplash_wallpaper.py
@@ -11,13 +11,11 @@ SEARCH_STRING="wallpaper"
 MACOS_DISPLAY_RESOLUTION="2880x1800"
 LINUX_DISPLAY_RESOLUTION="1920x1080"
 
-
 def get_os():
     uname_output=subprocess.run("uname", capture_output=True, text=True)
     # TODO remove all occurrences of following check and set a common env var
     if(uname_output.stdout.strip()=="Darwin"):
         os_arch="macos"
-    # TODO replace expr with python processing of uname command
     elif (uname_output.stdout.strip()=="Linux"):
         os_arch="linux"
     return os_arch
@@ -32,7 +30,6 @@ def get_display_resolution(os_arch):
 def download_image(display_resolution):
     random_int=random.randrange(10000)
     download_url=UNSPLASH_SOURCE_API_URL+display_resolution+"?"+SEARCH_STRING+"/"+str(random_int)
-    # print(download_url)
     response=requests.get(download_url)
     if (response.status_code==requests.codes.ok):
         redirect_url=response.url
@@ -60,8 +57,6 @@ def set_wallpaper_for_linux(filename):
 
 if __name__ == "__main__":
     os_arch=get_os()
-    # print(os)
     display_resolution=get_display_resolution(os_arch)
-    # print(display_resolution)
     filename=download_image(display_resolution)
     set_wallpaper(filename, os_arch)
